9e35b96d580d515f8011b007f6747eb16f4445d7
Timothy <Thron41k@gmail.com>
2024-10-04 14:13:15 +0800

    start

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите что-то");
+var str = Console.ReadLine();
+Console.WriteLine("Укажите глубину эха");
+var deep = int.Parse(Console.ReadLine() ?? "1");
+for (var i = 0; i < deep; i++)
+{
+    Echo(str);
+}
+return;
+
+static void Echo(string? saidword)
+{
+    Console.WriteLine(saidword);
+}
